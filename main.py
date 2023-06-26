8ef3ce059433a86e10301d555f0d98c6fc816081
Shreya <77922908+Predator21X-bot@users.noreply.github.com>
2023-06-26 08:52:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# API key obtained from the Adobe PDF Extract API serices.The requests library needs to be installed (pip install requests) as it is used to make the HTTP requests.
+
+# In this example, the extract_invoice_data function takes the URL of a PDF invoice as input and makes a POST request to the Adobe PDF Services API. It specifies the options to extract the table from the invoice while excluding the header and footer. The extracted table rows are then returned as a result.
+
 import requests
 import json
 
 # Adobe PDF Services credentials
-api_key = '6b15ec8b9619450698eef2295607a789'
+api_key = 'e2c3a983a8d345129e5feae80b286380'
 base_url = 'https://pdfservices.adobe.com'
 
 # curl -X POST 'https://ims-na1.adobelogin.com/ims/token/v3' -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&client_id=6b15ec8b9619450698eef2295607a789&client_secret=p8e-K6D70g74dowjhoIxN4DJh09APV6rVxND&scope=openid,AdobeID,DCAPI'
@@ -37,7 +41,7 @@ def extract_invoice_data(pdf_url):
     else:
         print(f"Error extracting data: {response.text}")
 
-# Example usage
+# Example usage - replaced by sampleInputs as well
 pdf_invoice_url = 'https://example.com/invoice.pdf'
 invoice_data = extract_invoice_data(pdf_invoice_url)
 
