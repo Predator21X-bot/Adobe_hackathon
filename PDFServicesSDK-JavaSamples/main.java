ef4e1c39dfcd3c104497118d92b856f190d553a1
Shreya <77922908+Predator21X-bot@users.noreply.github.com>
2023-06-26 08:42:27 +0530

    Add files via upload

---
+++ PDFServicesSDK-JavaSamples/main.java
@@ -0,0 +1,56 @@
+//In this API - KEY represents my API credentials obtained from Adobe PDF Services. Additionally, I added and included the OkHttp library as a project dependency. mainly using Maven, and downloading the JAR file.
+
+//In this example, the extractInvoiceData function takes the URL of a PDF invoice as input and sends a POST request to the Adobe PDF Services API. The request includes the necessary headers, payload, and API key. The extracted data is printed to the console for demonstration purposes.
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class InvoiceExtractor {
+    // Adobe PDF Services credentials
+    private static final String API_KEY = "6b15ec8b9619450698eef2295607a789";
+    private static final String BASE_URL = "https://pdfservices.adobe.com";
+
+    public static void main(String[] args) {
+        // Example usage
+        String pdfInvoiceUrl = "https://example.com/invoice.pdf";
+        extractInvoiceData(pdfInvoiceUrl);
+    }
+
+    // Function to extract data from a PDF invoice
+    private static void extractInvoiceData(String pdfUrl) {
+        // API endpoint
+        String endpoint = "/pdfservices/v1/documents";
+
+        // Request headers
+        Headers headers = new Headers.Builder()
+                .add("Content-Type", "application/json")
+                .add("x-api-key", API_KEY)
+                .build();
+
+        // Request payload
+        String requestBody = "{\"url\":\"" + pdfUrl + "\",\"options\":{\"extract:table\":true,\"extract:footer\":false,\"extract:header\":false}}";
+
+        // Create OkHttp client and request
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(BASE_URL + endpoint)
+                .headers(headers)
+                .post(RequestBody.create(requestBody, MediaType.parse("application/json")))
+                .build();
+
+        // Make the API request
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                // Process the extracted data
+                System.out.println(responseBody);
+            } else {
+                System.out.println("Error extracting data: " + response.body().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
